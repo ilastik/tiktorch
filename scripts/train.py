15cb2f453c6cb7be74a62ab088dcdbd3f57afdbb
nrahaman <nasim.rahaman@iwr.uni-heidelberg.de>
2017-07-22 15:53:51 +0200

    - training script

---
+++ scripts/train.py
@@ -0,0 +1,40 @@
+"""Script to train on CREMI"""
+
+from neurofire.datasets.cremi.loaders import get_cremi_loaders
+from inferno.trainers.basic import Trainer
+from inferno.trainers.callbacks.logging.tensorboard import TensorboardLogger
+from tiktorch.models.dunet import DUNet
+from os.path import join
+
+
+# Parameters
+PROJECT_DIRECTORY = '/export/home/nrahaman/Python/Repositories/tiktorch/projects/DUNET-0'
+CONFIG_FILENAME = 'data_config.yml'
+
+
+def main():
+    # Load dataset
+    train_loader = get_cremi_loaders(config=join(PROJECT_DIRECTORY,
+                                                 'Configurations',
+                                                 CONFIG_FILENAME))
+    # Build model
+    dense_unet = DUNet(1, 1)
+    # Build trainer
+    trainer = Trainer(model=dense_unet) \
+        .build_optimizer('Adam') \
+        .build_criterion('SorensenDiceLoss') \
+        .build_logger(TensorboardLogger(send_image_at_batch_indices=0,
+                                        send_image_at_channel_indices='all',
+                                        log_images_every=(20, 'iterations')),
+                      log_directory=join(PROJECT_DIRECTORY, 'Logs')) \
+        .save_every((1000, 'iterations'), to_directory=join(PROJECT_DIRECTORY, 'Weights')) \
+        .set_max_num_iterations(1000000) \
+        .cuda()
+    # Bind loader to trainer
+    trainer.bind_loader('train', train_loader)
+    # Go!
+    trainer.fit()
+
+
+if __name__ == '__main__':
+    main()
