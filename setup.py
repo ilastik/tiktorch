2481f86465bd9080f58784e3f95ef3d676b452eb
k-dominik <k-dominik@users.noreply.github.com>
2018-11-27 15:06:27 +0100

    Added basic conda recipe
    This recipe uses conda's build variants to build two packages with different requirements:
    
    - tiktorch (pytorch)
    - tiktorchcpu (-> pytorch-cpu)
    
    I'll try to get the inferno ppl to adopt something similar if possible :)

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tiktorch',
+    url='https://github.com/ilastik/tiktorch',
+    author='ilastik team',
+    author_email='team@ilastik.org',
+    description='ilastik - pytorch bridge',
+    packages=find_packages(),
+)
