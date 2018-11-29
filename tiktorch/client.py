4a3e4512bbe742b7994a9b83f69cfe5a16f9009a
Nasim Rahaman <nasim.rahaman@iwr.uni-heidelberg.de>
2018-11-30 00:48:46 +0100

    - client and server with working forward under lab conditions

---
+++ tiktorch/client.py
@@ -0,0 +1,191 @@
+import os
+import logging
+import subprocess
+import yaml
+import zmq
+import sys
+
+import numpy as np
+import torch
+import torch.distributed as dist
+
+from tiktorch.tio import TikIn
+import tiktorch.utils as utils
+
+logging.basicConfig(level=logging.INFO)
+
+
+class TikTorchClient(object):
+    RANK = 0
+    SIZE = 2
+
+    _START_PROCESS = True
+
+    def __init__(self, build_directory, address='127.0.0.1', port='29500', meta_port='29501'):
+        self.build_directory = build_directory
+        self.addr = address
+        self.port = port
+        self.meta_port = meta_port
+        # Privates
+        self._args: list = None
+        self._process: subprocess.Popen = None
+        self._config = {}
+        self._zmq_context = None
+        self._zmq_socket = None
+        # Initialize
+        self.read_config()
+        self.init()
+
+    def init(self):
+        logger = logging.getLogger('TikTorchClient.init')
+        os.environ['MASTER_ADDR'] = self.addr
+        os.environ['MASTER_PORT'] = self.port
+        # Build args for the server
+        self._args = [
+            'python',
+            os.path.join(os.path.dirname(os.path.realpath(__file__)), 'server.py'),
+            self.build_directory,
+            self.addr,
+            self.port,
+            self.meta_port
+        ]
+        # Start server
+        if self._START_PROCESS:
+            logger.info("Starting Server...")
+            self._process = subprocess.Popen(self._args, stdout=sys.stdout)
+        # Init torch distributed
+        logger.info("Initializing Process Group...")
+        dist.init_process_group(backend='tcp', rank=self.RANK, world_size=self.SIZE)
+        # Make server for zmq
+        logger.info("Setting up ZMQ Context...")
+        self._zmq_context = zmq.Context()
+        logger.info("Setting up ZMQ Socket...")
+        self._zmq_socket = self._zmq_context.socket(zmq.PAIR)
+        logger.info("Binding to socket...")
+        self._zmq_socket.bind(f'tcp://{self.addr}:{self.meta_port}')
+        return self
+
+    def terminate(self):
+        self._process.terminate()
+        return self
+
+    def kill(self):
+        self._process.kill()
+        return self
+
+    def is_running(self):
+        return self._process.poll() is None
+
+    def read_config(self):
+        config_file_name = os.path.join(self.build_directory, 'tiktorch_config.yml')
+        if not os.path.exists(config_file_name):
+            raise FileNotFoundError(f"Config file not found in "
+                                    f"build_directory: {self.build_directory}.")
+        with open(config_file_name, 'r') as f:
+            self._config.update(yaml.load(f))
+        return self
+
+    def get(self, tag, default=None, assert_exist=False):
+        if assert_exist:
+            assert tag in self._config, f"Tag '{tag}' not found in configuration."
+        return self._config.get(tag, default)
+
+    def meta_send(self, info_dict):
+        self._zmq_socket.send_json(info_dict)
+        return self
+
+    def meta_recv(self):
+        return self._zmq_socket.recv_json()
+
+    def batch_inputs(self, inputs):
+        input_shapes = self.get('input_shape', assert_exist=True)
+        assert isinstance(input_shapes, (list, tuple))
+        # input_shapes can either be a list of shapes or a shape. Make sure it's the latter
+        if isinstance(input_shapes[0], int):
+            input_shapes = [input_shapes] * len(inputs)
+        elif isinstance(input_shapes[0], (list, tuple)):
+            pass
+        else:
+            raise TypeError(f"`input_shapes` must be a list/tuple of ints or "
+                            f"lists/tuples or ints. Got list/tuple of {type(input_shapes[0])}.")
+        utils.assert_(len(input_shapes) == len(inputs),
+                      f"Expecting {len(inputs)} inputs, got {len(input_shapes)} input shapes.",
+                      ValueError)
+        batches = [input.batcher(input_shape)
+                   for input, input_shape in zip(inputs, input_shapes)]
+        return batches
+
+    def parse_inputs(self, inputs):
+        if isinstance(inputs, TikIn):
+            inputs = [inputs]
+        elif isinstance(inputs, (np.ndarray, torch.Tensor)):
+            inputs = [TikIn([inputs])]
+        elif isinstance(inputs, (list, tuple)):
+            utils.assert_(all(isinstance(input, TikIn) for input in inputs),
+                          "Inputs must all be TikIn objects.")
+        else:
+            raise TypeError("Inputs must be list TikIn objects.")
+        return inputs
+
+    def request_dispatch(self, mode):
+        self.meta_send({'id': f'DISPATCH.{mode.upper()}'})
+        response = self.meta_recv()
+        return response['id'] == f'DISPATCHING.{mode.upper()}'
+
+    def forward(self, inputs: list):
+        logger = logging.getLogger('TikTorchClient.forward')
+        # Send dispatch request and wait for confirmation
+        logger.info("Requesting dispatch...")
+        assert self.request_dispatch('FORWARD')
+        logger.info("Request successful.")
+        # Parse inputs
+        inputs = self.parse_inputs(TikIn(inputs))
+        # Batch inputs
+        batches = self.batch_inputs(inputs)
+        logger.info("Batched inputs.")
+        # Make info dict to send to server
+        info = {'id': 'FORWARD.BATCHSPEC',
+                'len': len(batches),
+                'shapes': tuple(batch.shape for batch in batches)}
+        logger.info("Sending BatchSpec.")
+        self.meta_send(info)
+        # Send batch to the server
+        for batch in batches:
+            logger.info("Sending batch.")
+            dist.send(batch, 1)
+        # Receive meta data
+        logger.info("Waiting for OutSpec.")
+        outspec = self.meta_recv()
+        assert outspec['id'] == 'FORWARD.OUTSPEC'
+        logger.info("OutSpec received.")
+        output_tensor = torch.zeros(*outspec['shape'])
+        # Receive it
+        dist.recv(tensor=output_tensor, src=1)
+        logger.info("Output received.")
+        # Convert to np and done
+        return output_tensor.numpy()
+
+    def train(self, *args, **kwargs):
+        pass
+
+
+def debug_client():
+    TikTorchClient.read_config = lambda self: self
+    TikTorchClient._START_PROCESS = False
+    client = TikTorchClient(build_directory='.', address='127.0.0.1', port='29500',
+                            meta_port='29501')
+    client._model = torch.nn.Conv2d(1, 1, 1)
+    client._config = {'input_shape': [1, 512, 512],
+                      'dynamic_input_shape': '(32 * (nH + 1), 32 * (nW + 1))'}
+    return client
+
+
+if __name__ == '__main__':
+    client = debug_client()
+    logging.info("Obtained client. Forwarding...")
+    out = client.forward([np.random.uniform(size=(512, 512)).astype('float32') for _ in range(1)])
+    logging.info(f"out.shape = {out.shape}")
+    logging.info("Forwarding again...")
+    out = client.forward([np.random.uniform(size=(512, 512)).astype('float32') for _ in range(1)])
+    logging.info(f"out.shape = {out.shape}")
+    logging.info("Done!")
---
+++ tiktorch/server.py
@@ -0,0 +1,243 @@
+import logging
+import os
+from importlib import util as imputils
+import zmq
+
+import numpy as np
+import torch
+import torch.distributed as dist
+import yaml
+
+from tiktorch.tio import TikIn, TikOut
+import tiktorch.utils as utils
+from tiktorch.device_handler import ModelHandler
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger('TikTorchServer')
+
+
+class TikTorchServer(object):
+    RANK = 1
+    SIZE = 2
+
+    def __init__(self, build_directory, address, port, meta_port, device=None):
+        # Privates
+        self._build_directory = None
+        self._handler: ModelHandler = None
+        self._model = None
+        self._config = {}
+        # Set up queues
+        self._zmq_context: zmq.Context = None
+        self._zmq_socket: zmq.Socket = None
+        self._zmq_pollin: zmq.Poller = None
+        if device is None:
+            # The default behaviour is to select a GPU if one is availabe.
+            # This can be overriden by providing device in the constructor.
+            self._device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+        else:
+            self._device = device
+        # Publics
+        self.build_directory = build_directory
+        self.addr = address
+        self.port = port
+        self.meta_port = meta_port
+        self.init()
+        self.read_config()
+
+    def init(self):
+        logger = logging.getLogger('TikTorchServer.init')
+        os.environ['MASTER_ADDR'] = self.addr
+        os.environ['MASTER_PORT'] = self.port
+        # Init torch distributed
+        logger.info("Initializing Process Group...")
+        dist.init_process_group(backend='tcp', rank=self.RANK, world_size=self.SIZE)
+        # Init ZMQ
+        logger.info("Setting up ZMQ Context...")
+        self._zmq_context = zmq.Context()
+        logger.info("Setting up ZMQ Socket...")
+        self._zmq_socket = self._zmq_context.socket(zmq.PAIR)
+        logger.info("Binding to socket...")
+        self._zmq_socket.connect(f'tcp://{self.addr}:{self.meta_port}')
+        logger.info("Setting up Poller...")
+        self._zmq_pollin = zmq.Poller()
+        self._zmq_pollin.register(self._zmq_socket, zmq.POLLIN)
+
+    def meta_send(self, info_dict):
+        self._zmq_socket.send_json(info_dict)
+        return self
+
+    def meta_recv(self):
+        return self._zmq_socket.recv_json()
+
+    @property
+    def output_shape(self):
+        return self.get('output_shape')
+
+    @property
+    def halo(self):
+        """
+        Returns the halo in dynamic base shape blocks
+        """
+        assert self.handler is not None
+        halo_block = self.handler.halo_in_blocks
+        base_shape = self.handler.dynamic_shape.base_shape
+        return [shape*block for shape, block in zip(base_shape, halo_block)]
+
+    @property
+    def build_directory(self):
+        if self._build_directory is not None:
+            return self._build_directory
+        else:
+            raise ValueError("Trying to access `build_directory`, but it's not set yet.")
+
+    @build_directory.setter
+    def build_directory(self, value):
+        if not os.path.exists(value):
+            raise FileNotFoundError(f"Build directory does not exist: {value}")
+        self._build_directory = value
+
+    @property
+    def model(self):
+        return self.handler.model
+
+    @property
+    def device(self):
+        return self.handler.device
+
+    @property
+    def handler(self):
+        if self._handler is None:
+            self.load_model()
+        return self._handler
+
+    def train(self, data, labels):
+        assert self.handler is not None
+        self.handler.train(data, labels)
+        pass
+
+    def dry_run(self, image_shape, train=False):
+        """
+        Initiates dry run.
+        Parameters
+        ----------
+        image_shape: list or tuple
+        shape of an image in the dataset (e.g `HW` for 2D or `DHW` for 3D)
+        """
+        assert self.handler is not None
+        return self.handler.binary_dry_run(list(image_shape), train_flag=train)
+
+    def read_config(self):
+        config_file_name = os.path.join(self.build_directory, 'tiktorch_config.yml')
+        if not os.path.exists(config_file_name):
+            raise FileNotFoundError(f"Config file not found in "
+                                    f"build_directory: {self.build_directory}.")
+        with open(config_file_name, 'r') as f:
+            self._config.update(yaml.load(f))
+        return self
+
+    def _set_handler(self, model):
+        assert self.get('input_shape') is not None
+        # Pass
+        self._handler = ModelHandler(model=model,
+                                     device_names=self._device,
+                                     channels=self.get('input_shape')[0],
+                                     dynamic_shape_code=self.get('dynamic_input_shape'))
+
+    def get(self, tag, default=None, assert_exist=False):
+        if assert_exist:
+            assert tag in self._config, f"Tag '{tag}' not found in configuration."
+        return self._config.get(tag, default)
+
+    def load_model(self):
+        # Dynamically import file.
+        model_file_name = os.path.join(self.build_directory, 'model.py')
+        module_spec = imputils.spec_from_file_location('model', model_file_name)
+        module = imputils.module_from_spec(module_spec)
+        module_spec.loader.exec_module(module)
+        # Build model from file
+        model: torch.nn.Module = \
+            getattr(module, self.get('model_class_name'))(**self.get('model_init_kwargs'))
+        # Load parameters
+        state_path = os.path.join(self.build_directory, 'state.nn')
+        try:
+            state_dict = torch.load(state_path, map_location=lambda storage, loc: storage)
+            model.load_state_dict(state_dict)
+        except:
+            raise FileNotFoundError(f"Model weights could not be found at location '{state_path}'!")
+        # Build handler
+        self._set_handler(model)
+        return self
+
+    def forward(self):
+        logger = logging.getLogger('TikTorchServer.forward')
+        logger.info("Receiving BatchSpec...")
+        batch_spec = self.meta_recv()
+        assert batch_spec['id'] == 'FORWARD.BATCHSPEC'
+        logger.info("Received BatchSpec.")
+        batches = [torch.zeros(*shape) for shape in batch_spec['shapes']]
+        for batch in batches:
+            logger.info("Receiving batch from chief.")
+            dist.recv(batch, src=0)
+        # Forward
+        logger.info("Feedforward.")
+        output_batches = self.handler.forward(*batches)
+        # Send output spec
+        logger.info("Sending OutSpec.")
+        self.meta_send({'id': 'FORWARD.OUTSPEC', 'shape': tuple(output_batches.shape)})
+        logger.info("Sending output.")
+        dist.send(output_batches, dst=0)
+        logger.info("Sent output.")
+
+    def listen(self):
+        logger = logging.getLogger('TikTorchServer.listen')
+        logger.info('Waiting...')
+        # Listen for requests
+        while True:
+            socks = dict(self._zmq_pollin.poll(50))
+            if socks:
+                # Yay, a message!
+                if socks.get(self._zmq_socket) == zmq.POLLIN:
+                    logger.info("Request Polled.")
+                    request = self.meta_recv()
+                    logger.info("Request Received.")
+                    if request['id'] == 'DISPATCH.FORWARD':
+                        logger.info("Received request to dispatch forward.")
+                        # Confirm dispatch
+                        self.meta_send({'id': 'DISPATCHING.FORWARD'})
+                        logger.info("Dispatch confirmed.")
+                        self.forward()
+                        logger.info("Forward successful; waiting...")
+                    elif request['id'] == 'DISPATCH.TRAIN':
+                        # Call train
+                        # TODO
+                        pass
+                    else:
+                        # Bad id
+                        raise RuntimeError
+
+
+def debug_server():
+    TikTorchServer.read_config = lambda self: self
+    server = TikTorchServer(build_directory='.', address='127.0.0.1', port='29500',
+                            meta_port='29501')
+    server._model = torch.nn.Conv2d(1, 1, 1)
+    server._config = {'input_shape': [1, 512, 512],
+                      'dynamic_input_shape': '(32 * (nH + 1), 32 * (nW + 1))'}
+    server._set_handler(server._model)
+    return server
+
+
+if __name__ == '__main__':
+    import argparse
+    parsey = argparse.ArgumentParser()
+    parsey.add_argument('build_directory', type=str)
+    parsey.add_argument('addr', type=str)
+    parsey.add_argument('port', type=str)
+    parsey.add_argument('meta_port', type=str)
+    # args = parsey.parse_args()
+    # Go!
+    server = debug_server()
+    # server = TikTorchServer(build_directory=args.build_directory, address=args.addr,
+    #                         port=args.port, meta_port=args.meta_port)
+    server.listen()
+
