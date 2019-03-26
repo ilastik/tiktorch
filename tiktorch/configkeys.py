4b9e76803a6715381387224e9c273ba3162459eb
fynnbe <thefynnbe@gmail.com>
2019-03-26 16:33:17 +0100

    add INFERENCE_SHAPE_UPPER_BOUND and INFERENCE_SHAPE_LOWER_BOUND to configkeys

--- tiktorch/configkeys.py
+++ tiktorch/configkeys.py
@@ -3,3 +3,6 @@ INPUT_CHANNELS = 'input_channels'
 TRAINING_SHAPE = 'training_shape'  # in tzyx, zyx, or yx (without batch_size and input_channels)
 TRAINING_SHAPE_UPPER_BOUND = "training_shape_upper_bound"  # same conventions as for TRAINING_SHAPE
 TRAINING_SHAPE_LOWER_BOUND = "training_shape_lower_bound"  # same conventions as for TRAINING_SHAPE
+
+INFERENCE_SHAPE_UPPER_BOUND = "inference_shape_upper_bound"  # same conventions as for TRAINING_SHAPE
+INFERENCE_SHAPE_LOWER_BOUND = "inference_shape_lower_bound"  # same conventions as for TRAINING_SHAPE
