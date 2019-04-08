b9b57eeab54b625ed296366186fa2faed39d5ab9
fynnbe <thefynnbe@gmail.com>
2019-04-08 15:11:02 +0200

    repair test_tiktypes

--- tests/test_tiktypes.py
+++ tests/test_tiktypes.py
@@ -1,16 +1,15 @@
 import torch
 
-from tiktorch.tiktypes import TikTensor, TikTensorBatch
+from tiktorch.tiktypes import LabeledTikTensor, LabeledTikTensorBatch
 
 
 def test_tt():
-    a = TikTensor(torch.zeros(5, 5))
-    assert isinstance(a.as_torch(with_label=True), tuple)
-    assert len(a.as_torch(with_label=True)) == 2
+    a = LabeledTikTensor(torch.zeros(5, 5), torch.zeros(5, 5))
+    assert isinstance(a.as_torch(), tuple)
+    assert len(a.as_torch()) == 2
 
 def test_ttb():
-    a = TikTensorBatch([TikTensor(torch.zeros(5, 5))])
-    assert isinstance(a.as_torch(with_label=True), list)
-    assert len(a.as_torch(with_label=True)) == 1
-    assert all([len(aa) == 2 for aa in a.as_torch(with_label=True)])
-
+    a = LabeledTikTensorBatch([LabeledTikTensor(torch.zeros(5, 5), torch.zeros(5, 5))])
+    assert isinstance(a.as_torch(), list)
+    assert len(a.as_torch()) == 1
+    assert all([len(aa) == 2 for aa in a.as_torch()])
