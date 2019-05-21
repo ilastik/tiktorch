1432d8a1e437c0bafba6b4944fb8831f6f36b71b
fynnbe <thefynnbe@gmail.com>
2019-05-21 10:22:37 +0200

    improve DynamicDataset

--- tiktorch/handler/datasets.py
+++ tiktorch/handler/datasets.py
@@ -1,9 +1,10 @@
 import itertools
 import torch
 
+from collections import OrderedDict
 from torch.utils.data.dataset import Dataset
 
-from tiktorch.tiktypes import LabeledTikTensorBatch, TikTensorBatch
+from tiktorch.tiktypes import LabeledTikTensorBatch
 
 
 class DynamicDataset(Dataset):
@@ -13,73 +14,67 @@ class DynamicDataset(Dataset):
     Comparison to the current generator implementation at: https://stackoverflow.com/a/47202816
     """
 
-    def __init__(self, data: LabeledTikTensorBatch = None, transforms=None, gamma: float = 0.9) -> None:
-        assert transforms is None or callable(transforms), "Given 'transforms' is not callable"
-        self.transforms = transforms
+    def __init__(self, data: LabeledTikTensorBatch = None, transform=None, gamma: float = 0.9) -> None:
+        assert transform is None or callable(transform), "Given 'transforms' is not callable"
+        self.transform = transform
         # the data dict holds values for each key, these values are typically a tuple of (raw img, label img)
         if data is None:
             data = LabeledTikTensorBatch([])
 
-        self.data = dict(zip(data.ids, data.as_torch()))
+        self.data = OrderedDict(zip(data.ids, data.as_torch()))
         # update counts keeps track of how many times a specific key has been added/updated
         self.weights = {key: 1.0 for key in data.ids}
         self.update_counts = {key: 1 for key in data.ids}
-        self.removed_in_this_epoch = set()
+        self.recently_removed = set()
         self.gamma = gamma
 
-    def __getitem__(self, index):
+    def __getitem__(self, index, update_weight=True):
         key, fetched = next(itertools.islice(self.data.items(), index, index + 1))
-        self.weights[key] *= self.gamma
+        if update_weight:
+            self.weights[key] *= self.gamma
 
-        if self.transforms is None:
+        if self.transform is None:
             return fetched
-        elif callable(self.transforms):
-            return self.transforms(*fetched)
         else:
-            raise RuntimeError
+            return self.transform(*fetched)
 
     def __len__(self):
         return len(self.data)
 
-    def update(self, data: TikTensorBatch, labels: TikTensorBatch) -> None:
+    def update(self, data: LabeledTikTensorBatch) -> None:
         """
         :param keys: list of keys to identify each value by
         :param values: list of new values. (remove from dataset if not bool(value). The count will be kept.)
         """
         # self.data.update(zip(keys, values))
         # update update counts
-        for d, l in zip(data, labels):
-            assert d.id == l.id
+        for d in data:
             key = d.id
             self.update_counts[key] = self.update_counts.get(key, 0) + 1
-            # remove deleted samples (values)
+            raw, label = d.as_torch()
 
-            # TODO: unreachable
-            # previously we had here followin line:
-            #
-            # key, value = d.id, d.as_torch()
-            #
-            # where as torch returned two element tuple
-            #
-            # if value
-            if False:
-                if key in self.data.keys():
-                    # del self.data[key]  # todo: make truly dynamic. problem: let sampler know
-                    self.data[key] = tuple([*self.data[key][:-1], torch.zeros_like(self.data[key][-1])])
-                    self.removed_in_this_epoch.add(key)
-                    self.weights[key] = 0
-            else:
-                self.data[key] = d.as_torch(), l.as_torch()
-                self.removed_in_this_epoch.discard(key)
+            if label.any():
+                # add sample-label pair to dataset
+                self.data[key] = raw, label
+                self.recently_removed.discard(key)
+                self.weights[key] = self.weights.get(key, 0) + 1  # todo: take update counts into account properly
+            elif key in self.data.keys():
+                # flag to remove sample-label pair from dataset
+                # del self.data[key]  # todo: make truly dynamic. problem: let sampler know
+                self.weights[key] = 0
+                self.recently_removed.add(key)
 
-                self.weights[key] = 1.0  # todo: take update counts into account
+                # the following line is obsolete. It's put here for safety (better to train on an empty patch than on
+                # an old label, if this patch somehow gets sampled anyway somehow) todo: remove after testing
+                self.data[key] = raw, label
 
     def reset_indices(self) -> torch.Tensor:
         """
         Removes deleted samples from the dataset.
         :return: new weights
         """
-        for deleted_key in self.removed_in_this_epoch:
+        for deleted_key in self.recently_removed:
             del self.data[deleted_key]
+            del self.weights[deleted_key]
 
         return torch.DoubleTensor([self.weights[key] for key in self.data.keys()])
