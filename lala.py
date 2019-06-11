5b8c57326e1be098a64ac92b0fd88a850637963d
Starlord <pranjalsharma.ism@gmail.com>
2019-06-11 14:18:30 +0200

    robo user first commit

---
+++ lala.py
@@ -0,0 +1,6 @@
+import yaml
+
+with open("tests/data/CREMI_DUNet_pretrained_new/robot_config.yml") as f:
+    config_dict = yaml.load(f)
+
+
---
+++ tiktorch/handler/script.py
@@ -0,0 +1,10 @@
+import sys
+sys.path.append('D:/Machine Learning/tiktorch/tests/data/CREMI_DUNet_pretrained_new')
+from mr_robot import *
+from model import DUNet2D
+
+
+
+	
+
+
