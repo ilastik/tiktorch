9326f14180df44358ea1fdae93dc970f9601ac54
Maksim Novikov <mnovikov.work@gmail.com>
2019-07-05 11:08:59 +0200

    Add batched executor to throttle responses

---
+++ tests/rpc/test_utils.py
@@ -0,0 +1,30 @@
+import time
+import random
+
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from tiktorch.rpc.utils import BatchedExecutor
+
+
+def test():
+    blocks = list(range(100))
+
+    def process_block(block_id):
+        time.sleep(random.random() * 0.1)
+        if random.random() < 0.2:
+            raise Exception()
+        return (block_id,)
+
+    futures = []
+
+    with ThreadPoolExecutor(max_workers=4) as ex:
+        batcher = BatchedExecutor(batch_size=20)
+
+        for id_ in blocks:
+            work = lambda *a, **kw: ex.submit(process_block, *a, **kw)
+            futures.append(batcher.submit(work, id_))
+
+        count = 0
+        for f in as_completed(futures):
+            count += 1
+
+    assert count == len(blocks)
