a4ae7121e696f472ae26568ea29f01ff50a9fbb9
Maksim Novikov <mnovikov.work@gmail.com>
2019-07-18 11:02:15 +0200

    Bump version: 19.7.0-alpha.0 → 19.7.0-alpha.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="tiktorch",
-    version="19.7.0-alpha.0",
+    version="19.7.0-alpha.1.0",
     description="Tiktorch client/server",
     long_description=long_description,
     long_description_content_type="text/markdown",
