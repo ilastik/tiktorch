15392284230c147d02165a1f90418023db08587b
fynnbe <thefynnbe@gmail.com>
2020-02-07 14:32:20 +0100

    remove Config

--- tiktorch/server/exemplum.py
+++ tiktorch/server/exemplum.py
@@ -1,19 +1,10 @@
 from dataclasses import dataclass
-from typing import Any, Dict, Sequence, Union
+from typing import Any, Sequence
 
-import ignite
 import torch
 
 from pybio.spec import node
 from pybio.spec.utils import get_instance
-
-
-@dataclass
-class Config:
-    # max_iterations: int
-    # batch_size: int
-    # num_iterations_per_update: int
-    warmstart: bool
 
 
 @dataclass
@@ -36,14 +27,18 @@ class InferenceOutput(IterationOutput):
 
 
 class Exemplum:
-    def __init__(self, pybio_model: node.Model, config: Union[Dict[str, Any], Config]):
+    def __init__(
+        self, pybio_model: node.Model, warmstart: bool = True, batch_size: int = 1, num_iterations_per_update: int = 2
+    ):
+        self.max_num_iterations = 0
+        self.iteration_count = 0
         self.devices = [torch.device("cpu")]
         spec = pybio_model.spec
         self.model = get_instance(pybio_model)
         self.model.to(self.devices[0])
         if spec.framework == "pytorch":
             assert isinstance(self.model, torch.nn.Module)
-            if config.warmstart:
+            if warmstart:
                 state = torch.load(spec.prediction.weights.source, map_location=self.devices[0])
                 self.model.load_state_dict(state)
         else:
@@ -68,16 +63,8 @@ class Exemplum:
         out = self._inference_step_function(batch)
         return out.prediction
 
-    @property
-    def max_num_iterations(self) -> int:
-        return 0
-
     def set_max_num_iterations(self, max_num_iterations: int) -> None:
-        return None
-
-    @property
-    def iteration_count(self) -> int:
-        return 0
+        self.max_num_iterations = max_num_iterations
 
     def set_break_callback(self):
         return NotImplementedError
