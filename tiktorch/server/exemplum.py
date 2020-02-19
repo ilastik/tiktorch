06edae895ed56bac56f428493f1542390f12c87d
fynnbe <thefynnbe@gmail.com>
2020-02-19 16:44:21 +0100

    use make_concatenated_apply

--- tiktorch/server/exemplum.py
+++ tiktorch/server/exemplum.py
@@ -1,22 +1,10 @@
-from dataclasses import dataclass
 from typing import Any, Sequence, List
 
-import numpy
 import torch
 
-from pybio.core.transformations.base import ConcatenatedPyBioTransformation
+from pybio.core.transformations.base import make_concatenated_apply
 from pybio.spec import nodes
 from pybio.spec.utils import get_instance
-
-
-@dataclass
-class IterationOutput:
-    network_output: List[Any]
-
-
-@dataclass
-class InferenceOutput(IterationOutput):
-    pass
 
 
 # @dataclass
@@ -56,15 +44,11 @@ class Exemplum:
         self,
         *,
         pybio_model: nodes.Model,
-        warmstart: bool = False,
         batch_size: int = 1,
-        max_num_iterations: int = 0,
         num_iterations_per_update: int = 2,
         _devices=Sequence[torch.device],
     ):
-        self.batch_size = batch_size
-        self.max_num_iterations = max_num_iterations
-        self.num_iterations_per_update = num_iterations_per_update
+        self.max_num_iterations = 0
         self.iteration_count = 0
         self.devices = _devices
         spec = pybio_model.spec
@@ -73,6 +57,8 @@ class Exemplum:
         if len(spec.inputs) != 1 or len(spec.outputs) != 1:
             raise NotImplementedError("Only single input, single output models are supported")
 
+        assert len(spec.inputs) == 1
+        assert len(spec.outputs) == 1
         _input = spec.inputs[0]
         _output = spec.outputs[0]
 
@@ -81,11 +67,11 @@ class Exemplum:
 
         if _check_batch_dim(self._internal_input_axes):
             self.input_axes = self._internal_input_axes[1:]
-            self._input_transform = _add_batch_dim
+            self._input_batch_dimension_transform = _add_batch_dim
             _input_shape = _input.shape[1:]
         else:
             self.input_axes = self._internal_input_axes
-            self._input_transform = _noop
+            self._input_batch_dimension_transform = _noop
             _input_shape = _input.shape
 
         self.input_shape = list(zip(self.input_axes, _input_shape))
@@ -94,11 +80,11 @@ class Exemplum:
 
         if _check_batch_dim(self._internal_output_axes):
             self.output_axes = self._internal_output_axes[1:]
-            self._output_transform = _remove_batch_dim
+            self._output_batch_dimension_transform = _remove_batch_dim
             _halo = _halo[1:]
         else:
             self.output_axes = self._internal_output_axes
-            self._output_transform = _noop
+            self._output_batch_dimension_transform = _noop
 
         self.halo = list(zip(self.output_axes, _halo))
 
@@ -106,30 +92,17 @@ class Exemplum:
         self.model.to(self.devices[0])
         if spec.framework == "pytorch":
             assert isinstance(self.model, torch.nn.Module)
-            if warmstart:
+            if spec.prediction.weights is not None:
                 state = torch.load(spec.prediction.weights.source, map_location=self.devices[0])
                 self.model.load_state_dict(state)
         else:
             raise NotImplementedError
 
-        self._prediction_preprocess = ConcatenatedPyBioTransformation(
-            [get_instance(trf) for trf in spec.prediction.preprocess]
-        ).apply
-        self._prediction_postprocess = ConcatenatedPyBioTransformation(
-            [get_instance(trf) for trf in spec.prediction.postprocess]
-        ).apply
+        self._prediction_preprocess = make_concatenated_apply([get_instance(tf) for tf in spec.prediction.preprocess])
+        self._prediction_postprocess = make_concatenated_apply([get_instance(tf) for tf in spec.prediction.postprocess])
         # inference_engine = ignite.engine.Engine(self._inference_step_function)
         # .add_event_handler(Events.STARTED, self.prepare_engine)
         # .add_event_handler(Events.COMPLETED, self.log_compute_time)
-
-    def _inference_step_function(self, batch) -> InferenceOutput:
-        assert all(isinstance(b, torch.Tensor) for b in batch)
-        batch = [b.to(self.devices[0]) for b in batch]
-        network_output = self.model(*batch)
-        if not isinstance(network_output, (list, tuple)):
-            network_output = [network_output]
-
-        return InferenceOutput(network_output=network_output)
 
     # def _validation_step_function(self) -> ValidationOutput:
     #     return ValidationOutput()
@@ -138,13 +111,14 @@ class Exemplum:
     # def _training_step_function(self) -> TrainingOutput:
     #     return TrainingOutput()
 
-    def forward(self, *batch) -> List[Any]:
-        with torch.no_grad():
-            batch = self._prediction_preprocess(*batch)
-            out = self._inference_step_function(batch)
-            batch = out.network_output
-            batch = self._prediction_postprocess(*batch)
-            return batch
+    def forward(self, batch) -> List[Any]:
+        batch = self._input_batch_dimension_transform(batch)
+        batch = self._prediction_preprocess(batch)
+        batch = [b.to(self.devices[0]) for b in batch]
+        batch = self.model(*batch)
+        batch = self._output_batch_dimension_transform(batch)
+        batch = self._prediction_postprocess(batch)
+        return batch
 
     def set_max_num_iterations(self, max_num_iterations: int) -> None:
         self.max_num_iterations = max_num_iterations
