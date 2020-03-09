89d2bdac1fb84462edf7c3eeefb436b5d1ae350a
Maksim Novikov <mnovikov.work@gmail.com>
2020-03-09 15:46:21 +0100

    Bump version: 20.3.0 → 20.3.1rc1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="tiktorch",
-    version="20.3.0",
+    version="20.3.1rc",
     description="Tiktorch client/server",
     long_description=long_description,
     long_description_content_type="text/markdown",
