aa52035e50f897dd7b3ea57f3a7acefaf23b1503
Maksim Novikov <mnovikov.work@gmail.com>
2021-03-11 12:30:44 +0100

    Update linux packaging script

--- scripts/package-linux.sh
+++ scripts/package-linux.sh
@@ -5,10 +5,26 @@ if [ -z "$1" ]; then
     echo "No version supplied";
     exit 1;
 fi
-VERSION=$1
+VERSION="$1"
+FLAVOR="${2:-cpu}"
+PACKAGE="tiktorch"
 
-conda create --yes --name tiktorch-server-env-pkg -c ilastik-forge -c pytorch -c conda-forge tiktorch="$VERSION"
-conda pack --name tiktorch-server-env-pkg --output "tiktorch-$VERSION-linux.tar" --arcroot "tiktorch-$VERSION"
-tar --transform "s/^/tiktorch-$VERSION\//" -rvf "tiktorch-$VERSION-linux.tar" -C "$ROOT_DIR" "run_tiktorch.sh"
+case $FLAVOR in
+  cuda)
+    PACKAGE="tiktorch"
+    ;;
+
+  cpu)
+    PACKAGE="tiktorch-cpu"
+    ;;
+
+  *)
+    echo -n "Unknown flavor: $FLAVOR"
+    exit 1;
+    ;;
+esac
+conda create --yes --name tiktorch-server-env-pkg -c ilastik-forge -c pytorch -c conda-forge  "$PACKAGE"="$VERSION"
+conda pack --name tiktorch-server-env-pkg --output "$PACKAGE-$VERSION-linux.tar" --arcroot "tiktorch"
+tar --transform "s/^/tiktorch\//" -rvf "$PACKAGE-$VERSION-linux.tar" -C "$ROOT_DIR" "run_tiktorch.sh"
 conda env remove --yes --name tiktorch-server-env-pkg
-pbzip2 --best --read "tiktorch-$VERSION-linux.tar"
+pbzip2 --best --read "$PACKAGE-$VERSION-linux.tar"
