3c3a6c2ca09694fd3449a88013e4b7a8291ac49c
k-dominik <k-dominik@users.noreply.github.com>
2022-03-31 17:36:45 +0200

    bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="tiktorch",
-    version="22.3.1",
+    version="22.3.2",
     description="Tiktorch client/server",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -25,8 +25,8 @@ setup(
     ],
     packages=find_packages(exclude=["tests"]),  # Required
     install_requires=[
-        "bioimageio.spec==0.4.5*",
-        "bioimageio.core==0.5.1*",
+        "bioimageio.spec==0.4.5.post6",
+        "bioimageio.core==0.5.3.post2",
         "grpcio-tools",
         "grpcio>=1.31",
         "numpy",
