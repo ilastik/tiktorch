dc88dbd1e83a15546135d8052544f9825e640151
k-dominik <k-dominik@users.noreply.github.com>
2023-02-22 08:45:10 +0100

    remove dev-only dep grpcio-tools - unavailable on arm-64

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="tiktorch",
-    version="23.2.1",
+    version="23.2.2",
     description="Tiktorch client/server",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -29,7 +29,6 @@ setup(
     install_requires=[
         "bioimageio.spec==0.4.9",
         "bioimageio.core==0.5.8",
-        "grpcio-tools",
         "grpcio>=1.31",
         "numpy",
         "protobuf",
