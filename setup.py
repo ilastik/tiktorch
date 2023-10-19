31a12b0db44907931807101ec7e7a5a8b835a568
k-dominik <k-dominik@users.noreply.github.com>
2023-10-19 15:36:24 +0200

    bump bioimage versions, bump versi;5Don

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="tiktorch",
-    version="23.7.0",
+    version="23.10.0",
     description="Tiktorch client/server",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -21,14 +21,14 @@ setup(
     classifiers=[  # Optional
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     packages=find_packages(exclude=["tests"]),  # Required
     install_requires=[
-        "bioimageio.spec==0.4.9",
-        "bioimageio.core==0.5.8",
+        "bioimageio.spec==0.4.9post4",
+        "bioimageio.core==0.5.9",
         "grpcio>=1.31",
         "numpy",
         "protobuf",
