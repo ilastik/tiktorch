9b0bdba5be4c07c252e553c602702fce52812b5d
k-dominik <k-dominik@users.noreply.github.com>
2024-12-11 10:15:05 +0100

    prepare release, bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="tiktorch",
-    version="23.11.0",
+    version="24.12.0",
     description="Tiktorch client/server",
     long_description=long_description,
     long_description_content_type="text/markdown",
